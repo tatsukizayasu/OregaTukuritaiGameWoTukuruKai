2a3a6879abb9aa34214447649ca9c803df794b47
杉山豪 <gou-sugiyama-k21f@stu.kbc.ac.jp>
2024-05-29 08:16:21 +0900

    0

--- Dodge hockey/Assets/Ball/Ball_test.cs
+++ Dodge hockey/Assets/Ball/Ball_test.cs
@@ -19,23 +19,34 @@
 
 //    }
 //}
+using UnityEngine;
+
 using System.Collections;
 using UnityEngine;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
 namespace Norikatuo.ReboundShot
 {
+    public enum BallCondition_test
+    {
+        stop,
+        Move,
+        CatchInit,
+        Catch,
+    }
+
     [RequireComponent(typeof(Rigidbody))]
     [RequireComponent(typeof(SphereCollider))]
+
+
     public class Ball_test : MonoBehaviour
     {
         [Tooltip("反発係数")]
-        [Range(0, 1)]
+        [Range(0, 2)]
         [SerializeField] private float bounciness = 1.0f;
+
         [Tooltip("弾の速度")]
         public float speed = 10.0f;
+
         [Tooltip("ONならDefaultContactOffsetの値を衝突検知に使用する")]
         [SerializeField] private bool useContactOffset = true;
 
@@ -47,21 +58,35 @@ namespace Norikatuo.ReboundShot
         private Vector3 lastDirection;
         private bool canKeepSpeed;
 
+        public Vector3 CatchBallLocathion;
+
+        private BallCondition_test ballCondition = BallCondition_test.stop;
+        public BallCondition_test BallCondition { get { return ballCondition; } set { ballCondition = value; } }   
+
         public void SetVelocity(Vector3 vector)
         {
-            rigidbody.velocity = vector * speed;
+                    rigidbody.velocity = vector * speed;
         }
 
+        //Start()前に実行　Rigidbody・SphereColliderを作成
         private void Awake()
         {
             rigidbody = GetComponent<Rigidbody>();
             sphereCollider = GetComponent<SphereCollider>();
         }
 
+        //Awake()後に実行
+        //初速を設定
         private void Start()
         {
             Init();
-            StartCoroutine(StartWaitForFixedUpdate());
+
+            Vector3 debugDirection;
+
+            debugDirection = new Vector3(Mathf.Cos(Mathf.Deg2Rad), Mathf.Sin(Mathf.Deg2Rad), 0f);
+            rigidbody.velocity = debugDirection * speed;
+
+
         }
 
         private void Init()
@@ -82,97 +107,94 @@ namespace Norikatuo.ReboundShot
             canKeepSpeed = true;
         }
 
-        private IEnumerator StartWaitForFixedUpdate()
-        {
-            while (true)
-            {
-                yield return new WaitForFixedUpdate();
-
-                WaitForFixedUpdate();
-            }
-        }
-
-        private void WaitForFixedUpdate()
-        {
-            KeepConstantSpeed();
-        }
-
-        /// <summary>
-        /// 速度を一定に保つ
-        /// 衝突や引っかかりによる減速を上書きする役目
-        /// </summary>
-        private void KeepConstantSpeed()
-        {
-            if (!canKeepSpeed) return;
-
-            var velocity = rigidbody.velocity;
-            var nowSqrSpeed = velocity.sqrMagnitude;
-            var sqrSpeed = speed * speed;
-
-            if (!Mathf.Approximately(nowSqrSpeed, sqrSpeed))
-            {
-                var dir = velocity != Vector3.zero ? velocity.normalized : lastDirection;
-                rigidbody.velocity = dir * speed;
-            }
-        }
 
         private void FixedUpdate()
         {
-            // 重なりを解消
-            OverlapDetection();
 
-            // 前フレームで反射していたら反射後速度を反映
-            ApplyReboundVelocity();
 
-            // 進行方向に衝突対象があるかどうか確認
-            ProcessForwardDetection();
 
-            // 1フレーム前の進行方向を保存
-            UpdateLastDirection();
-
-        }
-
-        /// <summary>
-        /// オブジェクトとの重なりを検知して解消するように位置補正する
-        /// 主にTransform.positionで移動してきた外部オブジェクトを回避するのに使う
-        /// </summary>
-        private void OverlapDetection()
-        {
-            // Overlap
-            var colliders = Physics.OverlapSphere(sphereCollider.transform.position, sphereCollider.radius);
-            var isOverlap = 1 < colliders.Length;
-            if (isOverlap)
+            switch (ballCondition)
             {
-                var pushVec = Vector3.zero;
-                var pushDistance = 0f;
-                var totalPushPos = Vector3.zero;
-                var pushCount = 0;
+                case BallCondition_test.stop:
+                    Debug.Log("stop");
 
-                foreach (var targetCollider in colliders)
-                {
-                    // 自身のコライダーなら無視する
-                    if (targetCollider == sphereCollider) continue;
+                    rigidbody.velocity = Vector3.zero;
 
-                    var isCollision = Physics.ComputePenetration(
-                        sphereCollider, sphereCollider.transform.position, sphereCollider.transform.rotation,
-                        targetCollider, targetCollider.transform.position, targetCollider.transform.rotation,
-                        out pushVec, out pushDistance);
-
-                    if (isCollision && pushDistance != 0)
+                    if (Input.GetKeyDown(KeyCode.R))
                     {
-                        totalPushPos += pushDistance * pushVec;
-                        pushCount++;
-                    }
-                }
+                        ballCondition = BallCondition_test.Move;
 
-                if (pushCount != 0)
-                {
-                    var pos = transform.position;
-                    pos += totalPushPos / pushCount;
-                    transform.position = pos;
-                }
+                        Vector3 debugDirection;
+                        debugDirection = new Vector3(Mathf.Cos(Mathf.Deg2Rad), Mathf.Sin(Mathf.Deg2Rad), 0f);
+                        rigidbody.velocity = debugDirection * speed;
+                    }
+                    break;
+                case BallCondition_test.Move:
+                    Debug.Log("Move");
+                    // 前フレームで反射していたら反射後速度を反映
+                    ApplyReboundVelocity();
+
+                    // 進行方向に衝突対象があるかどうか確認
+                    ProcessForwardDetection();
+
+                    //減速の制限
+                    KeepConstantSpeed();
+
+                    if (Input.GetKeyDown(KeyCode.R))
+                    {
+                        ballCondition = BallCondition_test.CatchInit;
+                    }
+
+                    break;
+                case BallCondition_test.CatchInit:
+                    //Debug.Log("CatchInit");
+
+                    //GameObject obj = GameObject.Find("Player1");
+                    //if (obj == null)
+                    //{
+                    //    Debug.LogError("Player1 オブジェクトが見つかりませんでした");
+                    //    break;
+                    //}
+                    //Vector3 locathion = obj.transform.position;
+                    //locathion.x += 10f;
+
+                    //CatchBallLocathion = locathion;
+
+                    ballCondition = BallCondition_test.Catch;
+
+                    break;
+                case BallCondition_test.Catch:
+
+                    Debug.Log("CatchInit");
+
+                    GameObject obj = GameObject.Find("Player1");
+                    if (obj == null)
+                    {
+                        Debug.LogError("Player1 オブジェクトが見つかりませんでした");
+                        break;
+                    }
+                    Vector3 locathion = obj.transform.position;
+                    locathion.x += 10f;
+
+                    CatchBallLocathion = locathion;
+
+
+
+                    Debug.Log("Catch");
+                    Debug.Log(CatchBallLocathion);
+                    this.transform.position = CatchBallLocathion;
+                    rigidbody.velocity = Vector3.zero;
+
+                    if (Input.GetKeyDown(KeyCode.R))
+                    {
+                        ballCondition = BallCondition_test.stop;
+                    }
+                    break;
+                    //this.transform.SetParent
             }
+
         }
+
 
         /// <summary>
         /// 計算した反射ベクトルを反映する
@@ -210,7 +232,6 @@ namespace Norikatuo.ReboundShot
                     var inVecDir = direction;
                     var outVecDir = Vector3.Reflect(inVecDir, normal);
                     outVecDir = new Vector3(outVecDir.x, 0.0f, outVecDir.z);
-                    Debug.Log(outVecDir);
                     reboundVelocity = outVecDir * speed * bounciness;
 
                     // 衝突予定先に接するように速度を調整
@@ -221,57 +242,26 @@ namespace Norikatuo.ReboundShot
             }
         }
 
-        private void UpdateLastDirection()
+        /// <summary>
+        /// 速度を一定に保つ
+        /// 衝突や引っかかりによる減速を上書きする役目
+        /// </summary>
+        private void KeepConstantSpeed()
         {
+            if (!canKeepSpeed) return;
+
             var velocity = rigidbody.velocity;
-            if (velocity != Vector3.zero)
+            var nowSqrSpeed = velocity.sqrMagnitude;
+            var sqrSpeed = speed * speed;
+
+            if (!Mathf.Approximately(nowSqrSpeed, sqrSpeed))
             {
-                lastDirection = velocity.normalized;
+                var dir = velocity != Vector3.zero ? velocity.normalized : lastDirection;
+                rigidbody.velocity = dir * speed;
             }
         }
 
-        #region Debug
-        private float debugAngle;
-        private Vector3 debugDirection;
-        private void Update()
-        {
-            //if (Input.GetKey(KeyCode.DownArrow))
-            //{
-            //    debugAngle -= 1f;
-            //}
-            //else if (Input.GetKey(KeyCode.UpArrow))
-            //{
-            //    debugAngle += 1f;
-            //}
-            //if (Input.GetKeyDown(KeyCode.Space))
-            //{
-            //    debugDirection = new Vector3(Mathf.Cos(debugAngle * Mathf.Deg2Rad), Mathf.Sin(debugAngle * Mathf.Deg2Rad), 0f);
-            //    rigidbody.velocity = debugDirection * speed;
-            //}
-        }
 
-#if UNITY_EDITOR
-        void OnDrawGizmos()
-        {
-            if (EditorApplication.isPlaying && rigidbody.velocity == Vector3.zero)
-            {
-                Gizmos.color = Color.green;
-                debugDirection = new Vector3(Mathf.Cos(debugAngle * Mathf.Deg2Rad), Mathf.Sin(debugAngle * Mathf.Deg2Rad), 0f);
-                var colliderRadius = sphereCollider.radius + (useContactOffset ? Physics.defaultContactOffset : 0);
-                var isHit = Physics.SphereCast(transform.position, colliderRadius, debugDirection * 10, out var hit);
-                if (isHit)
-                {
-                    Gizmos.DrawRay(transform.position, debugDirection * hit.distance);
-                    Gizmos.DrawWireSphere(transform.position + debugDirection * hit.distance, colliderRadius);
-                }
-                else
-                {
-                    Gizmos.DrawRay(transform.position, transform.position + debugDirection * 10);
-                }
-            }
-        }
-    
-#endif
-    #endregion
+
     }
 }
