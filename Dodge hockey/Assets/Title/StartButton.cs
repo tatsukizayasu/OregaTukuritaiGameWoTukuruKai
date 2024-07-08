30a74712a0772809295f2fa4cc1b23e274f2a6c0
gou-sugiyama <107020929+gou-sugiyama@users.noreply.github.com>
2024-07-08 14:51:08 +0900

    チュートリアルの方針変更
    マリオパーティのミニゲーム風チュートリアルを作る
    カメラの分割OK
    チュートリアルを実行するフラグをタイトルシーンから取得してGameManagerから発生させるボールの数を変化させる、壁を立てる、ゴールを作らないなどの分岐をさせる

--- Dodge hockey/Assets/Title/StartButton.cs
+++ Dodge hockey/Assets/Title/StartButton.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
 
 public class StartButton : MonoBehaviour
 {
---
+++ Dodge hockey/Assets/Tutorial/TutrialView.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutrialView : MonoBehaviour
+{
+    public Camera mainCamera;
+    public Camera secondaryCamera;
+
+    void Start()
+    {
+        // メインカメラのViewport Rectを設定 (左半分)
+        mainCamera.rect = new Rect(0, 0, 0.7f, 1);
+
+        // セカンダリカメラのViewport Rectを設定 (右半分)
+        secondaryCamera.rect = new Rect(0.5f, 0, 0.5f, 1);
+    }
+}
